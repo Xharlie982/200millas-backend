e930f733c1316d6469586bba6959ac50afa19c83
Mystic-W <guillaume.boussus@gmail.com>
2025-11-22 20:09:01 -0500

    fix

--- src/utils/responses.py
+++ src/utils/responses.py
@@ -1,11 +1,22 @@
 import json
+from decimal import Decimal
+
+def convert_decimal(obj):
+    if isinstance(obj, list):
+        return [convert_decimal(i) for i in obj]
+    elif isinstance(obj, dict):
+        return {k: convert_decimal(v) for k, v in obj.items()}
+    elif isinstance(obj, Decimal):
+        return float(obj)
+    return obj
 
 def response(status, body):
+    body = convert_decimal(body)
     return {
         "statusCode": status,
+        "body": json.dumps(body),
         "headers": {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-        },
-        "body": json.dumps(body),
+            "Access-Control-Allow-Origin": "*"
+        }
     }
